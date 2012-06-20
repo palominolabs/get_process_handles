a5e210300a0c8515a6d0ff61f552c048aabde353
Manuel Wudka-Robles <mwudka@me.com>
2012-06-19 21:41:35 -0700

    Argument parsing

--- main.c
+++ main.c
@@ -1,3 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static const char* USAGE = "Usage: %s pid\n";
+static const char* INVALID_PID = "Invalid pid: %s\n";
+
 int main(int argc, char **argv) {
+	// Figure out the PID the user's trying to get info about
+	if (argc != 2) {
+		printf(USAGE, argv[0]);
+		return 1;
+	}
+
+	int pid = atoi(argv[1]);
+	if (pid == 0) {
+		printf(INVALID_PID, argv[1]);
+		return 1;
+	}
+
+	printf("I would get information about %d\n", pid);
+
 	return 0;
 }
